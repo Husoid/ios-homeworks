27fd436a55c087a7427ae65abfef177080305e4c
Husoid <husoid@mail.ru>
2022-04-03 22:49:37 +0500

    Добавил @IBOoutlet'ы
    Добавил @IBOoutlet'ы для всех элементов.
    Загрузил в проект фотографию для аватара и установите ее для UIImageView

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by User on 03/04/2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+
+    @IBOutlet weak var fotoImageView: UIImageView!
+    @IBOutlet weak var fioLable: UILabel!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
