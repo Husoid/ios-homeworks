5f03f22a2ed3cbdda7a2f88030ab3c27240e0d35
Husoid <husoid@mail.ru>
2022-04-15 00:33:06 +0500

    Добавил кнопку

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -12,18 +12,25 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        makeButton()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func makeButton() {
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 150, height: 50))
+        button.center = view.center
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Открыть пост", for: .normal)
+        button.backgroundColor = .yellow
+        button.addTarget(self, action: #selector(clik), for: .touchUpInside)
+        view.addSubview(button)
+        
     }
-    */
+    
+    @objc private func clik() {
+        let postVC = PostViewController()
+        present(postVC, animated: true)
+        
+    }
 
 }
