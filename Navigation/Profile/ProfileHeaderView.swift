dd3304b157541f25abc9408c81228cd1a626c646
Husoid <husoid@mail.ru>
2022-04-30 22:54:06 +0500

    Сделал первую часть домашней работы

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    
+//    let widthSize = UIScreen.main.bounds.screens.size.width
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(imageView)
         addSubview(text)
+        addSubview(textStatus)
+        addSubview(button)
     }
     
     required init?(coder: NSCoder) {
@@ -22,7 +24,7 @@ class ProfileHeaderView: UIView {
     }
 
     let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 16, y: 16, width: 150, height: 150))
+        let imageView = UIImageView(frame: CGRect(x: 16, y: 16, width: 100, height: 100))
         imageView.layer.borderWidth = 3
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.image = UIImage(named: "изображение_viber_2020-11-07_12-39-49")
@@ -32,11 +34,46 @@ class ProfileHeaderView: UIView {
         return imageView
     } ()
     
-    let text: UITextField = {
-        let text = UITextField(frame: CGRect(x: 182, y: 27, width: 100, height: 50))
+    let text: UITextView = {
+        let text = UITextView(frame: CGRect(x: 132, y: 27, width: 300, height: 20))
         text.text = "Хусаинов Руслан"
         text.textColor = .black
-        text.font = UIFont.systemFont(ofSize: 18)
+        text.font = UIFont.boldSystemFont(ofSize: 18)
         return text
     } ()
+    
+    let textStatus: UITextView = {
+        let text = UITextView(frame: CGRect(x: 132, y: 80, width: 300, height: 20))
+        text.text = "Waiting for something..."
+        text.textColor = .gray
+        text.font = UIFont.systemFont(ofSize: 14)
+        return text
+    } ()
+    
+//    let textStatus: UITextField = {
+//        let text = UITextFild(frame: CGRect(x: 132, y: 80, width: 300, height: 20))
+//        text.text = "Waiting for something..."
+//        text.textColor = .gray
+//        text.font = UIFont.systemFont(ofSize: 14)
+//        return text
+//    } ()
+    
+    let button:UIButton = {
+        let button = UIButton(frame: CGRect(x: 16, y: 132, width: UIScreen.main.bounds.width - 32, height: 50))
+        button.backgroundColor = .blue
+        button.setTitle("Show status", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.shadowOffset.width = 4
+        button.layer.shadowOffset.height = 4
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    } ()
+    
+    @objc private func buttonPressed() {
+        print(textStatus.text!)
+    }
 }
