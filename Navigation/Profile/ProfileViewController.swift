0f04202d955e310e2784a3252c71397c08e75a2a
Husoid <husoid@mail.ru>
2022-05-09 17:09:11 +0500

    Добавил вторую кнопку

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -14,19 +14,42 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
     }
     
     override func viewWillLayoutSubviews() {
         makeProfileHeadrVC()
+        makeNewButton()
     }
 
     private func makeProfileHeadrVC () {
         let profileHeadrVC = ProfileHeaderView()
-        profileHeadrVC.frame = view.safeAreaLayoutGuide.layoutFrame
+        profileHeadrVC.translatesAutoresizingMaskIntoConstraints = false
         profileHeadrVC.backgroundColor = .lightGray
         view.addSubview(profileHeadrVC)
         
+        NSLayoutConstraint.activate([
+            profileHeadrVC.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            profileHeadrVC.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            profileHeadrVC.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            profileHeadrVC.heightAnchor.constraint(equalToConstant: 220)
+            
+        ])
+        
+    }
+    
+    private func makeNewButton () {
+        let newButton = UIButton()
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        newButton.setTitle("Новая кнопка", for: .normal)
+        view.addSubview(newButton)
+        
+        NSLayoutConstraint.activate([
+            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            newButton.heightAnchor.constraint(equalToConstant: 50)
+        
+        ])
     }
 
 }
